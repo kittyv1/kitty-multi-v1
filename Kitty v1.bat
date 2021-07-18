a326a3e47768c4bdfa3184e691a7e480c777e962
kittyv1 <87604927+kittyv1@users.noreply.github.com>
2021-07-18 12:03:47 +0000

    Add files via upload

---
+++ Kitty v1.bat
@@ -0,0 +1,80 @@
+@echo off
+chcp 65001
+:again
+cls
+color 5 
+title multi tool by kitty
+:back
+cls
+echo Checking for {Updates...]
+timeout 5 >nul
+cls
+echo whats up 
+echo.
+echo  ██ ▄█▀ ██▓▄▄▄█████▓▄▄▄█████▓▓██   ██▓
+echo  ██▄█▒ ▓██▒▓  ██▒ ▓▒▓  ██▒ ▓▒ ▒██  ██▒
+echo  ▓███▄░ ▒██▒▒ ▓██░ ▒░▒ ▓██░ ▒░  ▒██ ██░
+echo  ▓██ █▄ ░██░░ ▓██▓ ░ ░ ▓██▓ ░   ░ ▐██▓░
+echo  ▒██▒ █▄░██░  ▒██▒ ░   ▒██▒ ░   ░ ██▒▓░
+echo  ▒ ▒▒ ▓▒░▓    ▒ ░░     ▒ ░░      ██▒▒▒ 
+echo  ░ ░▒ ▒░ ▒ ░    ░        ░     ▓██ ░▒░ 
+echo  ░ ░░ ░  ▒ ░  ░        ░       ▒ ▒ ░░  
+echo  ░  ░    ░                     ░ ░
+pause
+set /p user=enter username:
+echo.
+set /P pass=Enter pass:
+if %user% ==kitty  if %pass% ==kitty goto main
+echo wrong log in try again
+timeout 10 
+goto again
+:main
+cls 
+echo.
+echo ██ ▄█▀ ██▓▄▄▄█████▓▄▄▄█████▓▓██   ██▓
+echo  ██▄█▒ ▓██▒▓  ██▒ ▓▒▓  ██▒ ▓▒ ▒██  ██▒
+echo ▓███▄░ ▒██▒▒ ▓██░ ▒░▒ ▓██░ ▒░  ▒██ ██░
+echo ▓██ █▄ ░██░░ ▓██▓ ░ ░ ▓██▓ ░   ░ ▐██▓░
+echo ▒██▒ █▄░██░  ▒██▒ ░   ▒██▒ ░   ░ ██▒▓░
+echo ▒ ▒▒ ▓▒░▓    ▒ ░░     ▒ ░░      ██▒▒▒ 
+echo ░ ░▒ ▒░ ▒ ░    ░        ░     ▓██ ░▒░ 
+echo ░ ░░ ░  ▒ ░  ░        ░       ▒ ▒ ░░  
+echo ░  ░    ░                     ░ ░      
+echo Welcome, Too Main Menu
+echo                                            1 - putty            2 - google 
+echo.
+echo                                            3 pinger             4 - fortnite Aimbot/wallhack
+echo 
+echo                                            5 Booters            6 - Pscan
+echo 
+echo                                            7 dowload dox        8 - best xxx site
+echo
+echo                                            9 reload menu
+:mainlogo
+set /p main=choose youre option... 
+if %main% == 1 start putty.exe
+if %main% == 2 start htttps://www.google.com/
+if %main% == 3 goto pinger 
+if %main% == 4 start https://nolur.com
+if %main% == 5 start https://www.vedbex.com/tools/home
+if %main% == 6 start pScan.exe
+if %main% == 7 start https://cdn.discordapp.com/attachments/865681625718980655/866263780848959508/Doxxer.exe
+if %main% == 8 start https://fuq.com
+if %main% == 9 goto :back
+goto mainlogo
+goto mainlogo
+:pinger
+start Lol.bat
+goto mainlogo
+:start seltoxpinger.bat
+goto mainlogo
+:google
+start https://www.google.com
+
+
+
+
+
+
+
+
